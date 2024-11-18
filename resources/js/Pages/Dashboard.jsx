bd37a413d593fe509759a0ff42ec54decbb90e22
aleksioz <aleksa.mejl@gmail.com>
2024-11-18 22:37:25 +0100

    Visual padding in layout fix

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -14,7 +14,7 @@ export default function Dashboard() {
         >
             <Head title="Seminar Works" />
 
-            <div className="py-12">
+            <div className="py-8">
                 <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
                     <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
                         <div className="p-6 text-gray-900 dark:text-gray-100">
