4c56ec2939f29c8396ea572cdad3d21abe9f7379
aleksioz <aleksa.mejl@gmail.com>
2024-12-05 14:17:25 +0100

    Add tooltip component for review quality

--- resources/js/Components/ReviewQuality.jsx
+++ resources/js/Components/ReviewQuality.jsx
@@ -1,6 +1,7 @@
 import { useForm } from '@inertiajs/react';
 import React, { useEffect, useRef } from 'react';
 import { Rating } from 'react-simple-star-rating';
+import { Tooltip } from 'react-tooltip';
 
 export default function ReviewQuality({
 	avgAssessment
@@ -20,7 +21,8 @@ export default function ReviewQuality({
 	}, [data]);
 	
 	return (
-		<div className="bg-white pt-2 dark:bg-gray-800 align-top shadow-md rounded-lg text-center">
+		<div data-tooltip-id="tooltip-instruction" data-tooltip-content="Click stars for your rating" className="bg-white pt-2 dark:bg-gray-800 align-top shadow-md rounded-lg text-center">
+			<Tooltip id="tooltip-instruction" />
 			<h4 className="pt-0 font-semibold leading-tight text-gray-800 dark:text-gray-200">
 				Review Quality
 			</h4>
