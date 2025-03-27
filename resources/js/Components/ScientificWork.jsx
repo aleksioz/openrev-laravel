d4da13139e52cc8b1a83370a346dffeb6e0ccc82
aleksioz <aleksa.mejl@gmail.com>
2025-03-27 20:29:34 +0100

    Enhance styling of ScientificWork component with improved margins and background color

--- resources/js/Components/ScientificWork.jsx
+++ resources/js/Components/ScientificWork.jsx
@@ -9,12 +9,13 @@ export default function ScientificWork({
 	id=''
 }) {
 	return (
-		<div className={`bg-white shadow-md rounded-lg p-6 text-center w-72 ${className}` }>
-			<h2 className="text-xl text-black font-bold mb-4">
+		<div className={`bg-white shadow-md rounded-lg text-center w-72 ${className}` }>
+			<h2 className="text-xl text-black font-bold mb-4 m-6">
 				<Link href={ route('scientificwork.show', id) }>{title}</Link>
 			</h2>
 			<p className="text-gray-700 mb-4">{author}</p>
-			<div className="text-gray-600 border-t-2">
+			<div className="bg-gray-200 rounded-lg text-gray-600 border-t-2">
+				<p>Published:</p>
 				<p>{info}</p>
 			</div>
 		</div>
